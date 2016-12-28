c3aa388fd144dc580016dbbaa7405e397d84c726
Cristian Angulo Nova <cristianangulonova@gmail.com>
2016-12-28 02:43:11 +0000

    Users

---
+++ src/AppBundle/Context/User/UserContext.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace AppBundle\Context\User;
+
+use Behat\Behat\Tester\Exception\PendingException;
+use Behat\Behat\Context\Context;
+
+class UserContext implements Context{
+    /**
+     * @Given I am logged in as an administrator
+     */
+    public function iAmLoggedInAsAnAdministrator()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When I want to add a new country
+     */
+    public function iWantToAddANewCountry()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When I choose :arg1
+     */
+    public function iChoose($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When I add it
+     */
+    public function iAddIt()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then I should be notified that it has been successfully created
+     */
+    public function iShouldBeNotifiedThatItHasBeenSuccessfullyCreated()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then the country :arg1 should appear in the store
+     */
+    public function theCountryShouldAppearInTheStore($arg1)
+    {
+        throw new PendingException();
+    }
+}
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="user", type="string", length=255)
+     */
+    private $user;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    private $description;
+
+
+    /**
+     * Get id.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set user.
+     *
+     * @param string $user
+     *
+     * @return User
+     */
+    public function setUser($user)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user.
+     *
+     * @return string
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * Set description.
+     *
+     * @param string $description
+     *
+     * @return User
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description.
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+}
